6195fbecbbc90badb52b12767c3ca38572eeb75c
윤용일 <yongyong1617@gmail.com>
2026-07-10 11:24:02 +0900

    feat: 스터디 만들기(살세 페이지)프론트 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx'
 import TodayHabitPage from './pages/TodayHabitPage.jsx';
-import DesignExample from './components/DesignExample.jsx';
+import StudyCreatePage from './pages/StudyCreatePage.jsx';
 
 function Main() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<DesignExample/>} />
+          <Route index element={<StudyCreatePage />} />
+          <Route path="study-create" element={<StudyCreatePage />} />
           <Route path="study/:id/habit" element={<TodayHabitPage />} />
         </Route>
       </Routes>
