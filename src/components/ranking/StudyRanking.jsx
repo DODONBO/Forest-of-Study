0b4424b498c8cc1759ec6857fdf211cf314a9904
haeun-chae <gkdms1399@gmail.com>
2026-07-16 14:25:04 +0900

    feat: 스터디 랭킹 API 연동

--- src/components/ranking/StudyRanking.jsx
+++ src/components/ranking/StudyRanking.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "../../utils/axios.js";
+
 function StudyRanking() {
+  const [studyRanking, setStudyRanking] = useState([]);
+
+  const getStudyRanking = async () => {
+    try {
+      const response = await axios.get("/ranking/study");
+      setStudyRanking(response.data);
+    } catch (error) {
+      console.error(error);
+    }  
+  };
+
+  const topStudyRankings = studyRanking.slice(0, 3);
+  const otherStudyRankings = studyRanking.slice(3, 10);
+
+  useEffect(() => {
+    getStudyRanking();
+  }, []);
+
   return (
     <div>
       <span>스터디 랭킹</span>
+      {topStudyRankings.map((study) => (
+        <div key={study.id}>
+          {study.rank}위 {study.name} {study.point}P
+        </div>
+      ))}
+      {otherStudyRankings.map((study) => (
+        <div key={study.id}>
+          {study.rank}위 {study.name} {study.point}P
+        </div>
+      ))}
     </div>
   )
 }
